066d7ed579aab1bb344c5c17763ee8e86a3bcc04
lijian11 <lijian11@meituan.com>
2015-05-07 11:54:55 +0800

    add test case & magic.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+document.getElementById('list').innerHTML = Magic('list-item-template').render({
+    data:[{
+        name:'magic1'    
+    },{
+        name:'magic2'    
+    },{
+        name:'magic3'    
+    }]    
+});
+
